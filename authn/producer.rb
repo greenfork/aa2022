8bdd102eaecc82fa2dee4ef04ff0e46db4909468
Dmitry Matveyev <dev@greenfork.me>
2022-05-10 19:46:13 +0600

    Producer improvement to cache serialization via json

--- authn/producer.rb
+++ authn/producer.rb
@@ -5,9 +5,10 @@ require_relative "broker"
 
 class Producer
   def self.call(event, topic:)
+    json_event = event.to_json
     puts "*" * 80
-    puts "Send to #{topic}: #{event.to_json}"
+    puts "Send to #{topic}: #{json_event}"
     puts "*" * 80
-    BROKER.produce_sync(topic:, payload: event.to_json)
+    BROKER.produce_sync(topic:, payload: json_event)
   end
 end
