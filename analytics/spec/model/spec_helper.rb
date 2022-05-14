b547219089db0d5781c96cb1914d83f9eea59f0f
Dmitry Matveyev <dev@greenfork.me>
2022-05-14 21:27:11 +0600

    Add templates for billing and analytics

---
+++ analytics/spec/model/spec_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+ENV["RACK_ENV"] = "test"
+require_relative "../../models"
+raise "test database doesn't end with test" unless DB.opts[:database] =~ /test\z/
+
+Sequel::Model.freeze_descendents
+DB.freeze
+
+require_relative "../minitest_helper"
